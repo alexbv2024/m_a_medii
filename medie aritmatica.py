fd43edcf6af0de554aa58fae01c4dfb093cc0b61
alexbv2024 <157834850+alexbv2024@users.noreply.github.com>
2024-01-26 20:11:21 +0200

    Add files via upload

---
+++ medie aritmatica.py
@@ -0,0 +1,7 @@
+def medie(a,b):
+    x=(a+b)/2
+    return x
+y=int(input("Care este primul nr pentru media aritmetica?"))
+z=int(input("Care este al doilea nr pentru media aritmetica?"))
+x=medie(y,z)
+print(x)
